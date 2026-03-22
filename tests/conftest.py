73de8d9b36f4bb12a56276b6dcb507bea99a00aa
Dmitry Tarasov <vizalyyy@gmail.com>
2026-03-23 03:10:30 +0500

    initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,58 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+base_url = "https://the-internet.herokuapp.com/login"
+valid_data = {
+    "username": "tomsmith",
+    "password": "SuperSecretPassword!",
+}
+invalid_data = {
+    "username": "Error",
+    "password": "Error",
+}
+massage_error = {
+    "username": "Your username is invalid!",
+    "password": "Your password is invalid!",
+}
+
+
+@pytest.fixture(scope="session")
+def driver():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+@pytest.fixture
+def valid_login(driver):
+    driver.get(base_url)
+    username_input = driver.find_element(By.ID, "username")
+    username_input.clear()
+    username_input.send_keys(valid_data["username"])
+
+    password_input = driver.find_element(By.ID, "password")
+    password_input.clear()
+    password_input.send_keys(valid_data["password"])
+
+    button_login = driver.find_element(By.CSS_SELECTOR, "button.radius")
+    button_login.click()
+    yield driver
+
+@pytest.fixture
+def login_page(driver):
+    driver.get(base_url)
+    yield driver
+
+@pytest.fixture(params=[
+    (invalid_data["username"], valid_data["password"], massage_error["username"]),
+    (valid_data["username"], invalid_data["password"], massage_error["password"]),
+])
+def invalid_login(driver, request):
+    return request.param
---
+++ tests/test_form.py
@@ -0,0 +1,73 @@
+from selenium.webdriver.common.by import By
+
+
+def test_successful_login(valid_login):
+    # driver.get("https://the-internet.herokuapp.com/login")
+
+    # username_input = driver.find_element(By.ID, "username")
+    # username_input.clear()
+    # username_input.send_keys("tomsmith")
+    #
+    # password_input = driver.find_element(By.ID, "password")
+    # password_input.clear()
+    # password_input.send_keys("SuperSecretPassword!")
+    #
+    # button_login = driver.find_element(By.CSS_SELECTOR, "button.radius")
+    # button_login.click()
+    driver = valid_login
+
+    flash = driver.find_element(By.ID, "flash-messages")
+    assert "You logged into a secure area!" in flash.text
+    header = driver.find_element(By.TAG_NAME, "h2")
+    assert "Secure Area" in header.text
+
+# def test_unsuccessful_login_name(driver):
+#     driver.get("https://the-internet.herokuapp.com/login")
+#
+#     username_input = driver.find_element(By.ID, "username")
+#     username_input.clear()
+#     username_input.send_keys("Error")
+#
+#     password_input = driver.find_element(By.ID, "password")
+#     password_input.clear()
+#     password_input.send_keys("SuperSecretPassword!")
+#
+#     button_login = driver.find_element(By.CSS_SELECTOR, "button.radius")
+#     button_login.click()
+#
+#     flash = driver.find_element(By.ID, "flash-messages")
+#     assert "Your username is invalid!" in flash.text
+#
+# def test_unsuccessful_login_password(driver):
+#     driver.get("https://the-internet.herokuapp.com/login")
+#
+#     username_input = driver.find_element(By.ID, "username")
+#     username_input.clear()
+#     username_input.send_keys("tomsmith")
+#
+#     password_input = driver.find_element(By.ID, "password")
+#     password_input.clear()
+#     password_input.send_keys("Error")
+#
+#     button_login = driver.find_element(By.CSS_SELECTOR, "button.radius")
+#     button_login.click()
+#
+#     flash = driver.find_element(By.ID, "flash-messages")
+#     assert "Your password is invalid!" in flash.text
+
+def test_unsuccessful_login(login_page, invalid_login):
+    driver = login_page
+    username, password, expected_message = invalid_login
+    username_input = driver.find_element(By.ID, "username")
+    username_input.clear()
+    username_input.send_keys(username)
+
+    password_input = driver.find_element(By.ID, "password")
+    password_input.clear()
+    password_input.send_keys(password)
+
+    button_login = driver.find_element(By.CSS_SELECTOR, "button.radius")
+    button_login.click()
+
+    flash = driver.find_element(By.ID, "flash-messages")
+    assert expected_message in flash.text
